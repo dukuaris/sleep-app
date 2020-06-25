926c77f7022c76331100a372189d733b211eeee3
Duke Yoo <dukuaris@gmail.com>
2020-06-25 17:35:57 +0900

    Add music

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+const audio = document.getElementById('audio');
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
 breathAnimation();
+
+audio.play();
 
 function breathAnimation() {
   text.innerText = '숨을 들이쉬세요!';
